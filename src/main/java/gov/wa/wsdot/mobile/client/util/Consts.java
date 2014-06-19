7a56427cd6cae091667598d6fd70276f1829b052
Wayne Dyck <dyckw@wsdot.wa.gov>
2014-06-19 15:39:21 -0700

    Bump version number.

--- src/main/java/gov/wa/wsdot/mobile/client/util/Consts.java
+++ src/main/java/gov/wa/wsdot/mobile/client/util/Consts.java
@@ -31,7 +31,7 @@ public final class Consts {
 	/**
 	 * Current version number and build of the app.
 	 */
-	public static final String APP_VERSION = "2.0.3";
+	public static final String APP_VERSION = "2.1.0";
 	
 	/**
 	 * The caller references the constants using <tt>Consts.EMPTY_STRING</tt>,
